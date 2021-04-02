773bc3d920c35c23b7785da1e1ea5160b0a04933
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-04-02 12:04:11 -0400

    Fix style and layout Rubocop offenses

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,9 +1,9 @@
 module ArticlesHelper
-  def show_author(article) 
-    if user_signed_in? 
-        article
-   else
-    "You need to sign up or login to see the author"
+  def show_author(article)
+    if user_signed_in?
+      article
+    else
+      'You need to sign up or login to see the author'
    end
   end
 end
--- app/helpers/partials_helper.rb
+++ app/helpers/partials_helper.rb
@@ -1,9 +1,9 @@
 module PartialsHelper
-    def session_partials
-        if user_signed_in?
-            render partial: 'partials/signedin'
-        else
-            render partial: 'partials/notsignedin'
-        end
+  def session_partials
+    if user_signed_in?
+      render partial: 'partials/signedin'
+    else
+      render partial: 'partials/notsignedin'
     end
+  end
 end
